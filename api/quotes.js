bed54eae9167efe783c863f4decc1414f9165593
FabianJimenez29 <fabian.jimenez5@ulatina.net>
2025-08-23 23:50:24 -0600

    Cambios produccion

--- api/quotes.js
+++ api/quotes.js
@@ -64,7 +64,7 @@ export default async function handler(req, res) {
     const { data, error } = await supabase
       .from('quotes')
       .select('*')
-      .eq('date', date)
+      .eq('fecha', date)
       .order('time', { ascending: true });
     if (error) return res.status(400).json({ error: error.message });
     return res.status(200).json({ quotes: data });

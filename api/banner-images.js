53cc48e9ddbfe1f3249c21c56cad492be0eb9e29
FabianJimenez29 <fabian.jimenez5@ulatina.net>
2025-09-17 15:57:57 -0600

    acacacaacac

--- api/banner-images.js
+++ api/banner-images.js
@@ -17,7 +17,6 @@ export default async function handler(req, res) {
     return res.status(200).end();
   }
 
-  // Solo permitir solicitudes GET
   if (req.method !== 'GET') {
     return res.status(405).json({ error: 'Método no permitido' });
   }
